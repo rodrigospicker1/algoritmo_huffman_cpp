4bc563a6e00497dbfc1eac5a5b34539ee5827138
rodrigospicker1 <r.spicker13@gmail.com>
2024-05-26 22:25:22 -0300

    aula_283

--- algoritmo.cpp
+++ algoritmo.cpp
@@ -18,6 +18,20 @@ using namespace std;
 
 #define TAM 256
 
+struct No
+{
+    unsigned char caracter;
+    int frequencia;
+    No *esq;
+    No *dir;
+    No *proximo;
+};
+
+struct Lista{
+    No *inicio;
+    int tam;
+};
+
 void inicializa_tabela_com_zero(unsigned int tab[])
 {
     for(int i = 0; i < TAM; i++)
@@ -48,16 +62,88 @@ void imprime_tab_frequencia(unsigned int tab[])
     }
 }
 
+void criar_lista(Lista *lista)
+{
+    lista->inicio = nullptr;
+    lista->tam = 0;
+}
+
+void inserir_ordenado(Lista *lista, No *no){
+    No *aux;
+    // a lista esta vazia?
+    if(lista -> inicio == NULL)
+    {
+        lista -> inicio = no;
+    }
+    //tem freqencia menor que o incio da lista ?
+    else if(no -> frequencia < lista -> inicio -> frequencia)
+    {
+        no -> proximo = lista ->inicio;
+        lista->inicio = no;
+    }
+    else
+    {
+        aux = lista -> inicio;
+        while(aux -> proximo && aux -> proximo -> frequencia <= no -> frequencia)
+            aux = aux -> proximo;
+        no -> proximo = aux -> proximo;
+        aux -> proximo = no;
+
+    }
+    lista -> tam++;
+}
+
+void preencher_lista(unsigned int tab[],Lista *lista)
+{
+    int i;
+    No *novo;
+    for(i = 0; i < TAM; i++)
+        if(tab[i]>0)
+        {
+            novo = (No*)malloc(sizeof(No));
+            if(novo)
+            {
+                novo -> caracter = i;
+                novo -> frequencia = tab[i];
+                novo -> dir = NULL;
+                novo -> esq = NULL;
+                novo -> proximo = NULL;
+
+                inserir_ordenado(lista,novo);
+            }
+            else
+            {
+                cout<<"\n ERRO ao alocar memoria em preencher_lista!\n";
+                break;
+            }
+        }
+}
+
+void imprimir_lista(Lista *lista){
+    No *aux  = lista->inicio;
+
+    cout<<"\nLista ordenada: Tamanho: "<<lista->tam;
+    while(aux){
+        cout<<"\nCaracter: "<<aux->caracter<<" Frequência: "<<aux->frequencia;
+        aux = aux->proximo;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     setlocale(LC_ALL,"");
 
     unsigned char texto[] = "Vamos aprender a programa";
     unsigned int tabela_frequencia[TAM];
+    Lista lista;
 
     inicializa_tabela_com_zero(tabela_frequencia);
     preenche_tab_frequencia(texto, tabela_frequencia);
     imprime_tab_frequencia(tabela_frequencia);
+
+    criar_lista(&lista);
+	preencher_lista(tabela_frequencia, &lista);
+	imprimir_lista(&lista);
 
     return 0;
 }
